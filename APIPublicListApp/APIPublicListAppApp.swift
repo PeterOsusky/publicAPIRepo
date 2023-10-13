808ba7665c48e92f537a2e290f9658f09c64db82
Peter Osusky <peterosusky94@gmail.com>
2023-10-13 11:57:00 +0200

    Initial Commit

---
+++ APIPublicListApp/APIPublicListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIPublicListAppApp.swift
+//  APIPublicListApp
+//
+//  Created by Peter on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct APIPublicListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
